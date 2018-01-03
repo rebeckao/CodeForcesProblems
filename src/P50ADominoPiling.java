e7e57adc181f9482677c3c1514661c4f1b395358
Johan Kvastad <jkvastad@gmail.com>
2018-01-03 18:57:30 +0100

    50A Domino Piling

---
+++ src/P50ADominoPiling.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class P50ADominoPiling {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int m = in.nextInt();
+        int n = in.nextInt();
+
+        System.out.println((m*n)/2);
+    }
+}
