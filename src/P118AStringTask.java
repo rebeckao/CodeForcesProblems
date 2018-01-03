5603e7f4ee24298b3e5b43797dc3c4fc319730ba
Johan Kvastad <jkvastad@gmail.com>
2018-01-03 18:41:23 +0100

    P118A String Task

---
+++ src/P118AStringTask.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class P118AStringTask {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String input = in.nextLine();
+
+        String answer = input
+                .toLowerCase()
+                .replaceAll("[aoyeui]", "")
+                .replaceAll("(.)", ".$1");
+
+        System.out.println(answer);
+    }
+}
